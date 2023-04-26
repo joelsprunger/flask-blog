b16fd1f28f12c289926db7e89522cf10dae0af78
Joel Sprunger <joelsprunger@Joels-MacBook-Pro.local>
2023-04-26 14:44:36 -0700

    adds forms and picture_handler

---
+++ companyblog/users/forms.py
@@ -0,0 +1,51 @@
+from ..models import User
+from .. import db
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("User Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), EqualTo("pw_confirm")])
+    pw_confirm = PasswordField("Confirm Password", validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+    @staticmethod
+    def check_email(self, field):
+        stmt = db.select(User).where(User.email == field.data)
+        if db.session.execute(stmt).first():
+            raise ValidationError("Your email has already been registered")
+
+    @staticmethod
+    def check_username(self, field):
+        stmt = db.select(User).where(User.username == field.data)
+        if db.session.execute(stmt).first():
+            raise ValidationError("Your username has already been registered")
+
+
+class UpdateUserForm(FlaskForm):
+    username = StringField("User Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    file = FileField("Upload a profile picture", FileAllowed(["jpg", "png"]))
+    submit = SubmitField("Update")
+    @staticmethod
+    def check_email(self, field):
+        stmt = db.select(User).where(User.email == field.data)
+        if db.session.execute(stmt).first():
+            raise ValidationError("Your email has already been registered")
+
+    @staticmethod
+    def check_username(self, field):
+        stmt = db.select(User).where(User.username == field.data)
+        if db.session.execute(stmt).first():
+            raise ValidationError("Your username has already been registered")
