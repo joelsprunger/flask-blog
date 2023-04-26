1ad8b7c6e32967de47005a16edbd809e9c31e5a2
Joel Sprunger <joelsprunger@Joels-MacBook-Pro.local>
2023-04-26 10:20:50 -0700

    Adds error pages, index, and info

--- companyblog/__init__.py
+++ companyblog/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+from . core.views import core
+from . error_pages.handlers import error_pages
+
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
